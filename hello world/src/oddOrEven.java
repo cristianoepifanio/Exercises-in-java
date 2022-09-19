aaa5aeea92b614c9696e5b0003acc49ff81514c3
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-19 09:23:51 -0300

    creating the first projects

---
+++ hello world/src/oddOrEven.java
@@ -0,0 +1,10 @@
+public class oddOrEven {
+    public static void main(String[] args) {
+        int num=5;
+        if(num%2==0){
+          System.out.println("This number is even.");
+        }else{
+            System.out.println("This number is odd.");
+        }
+    }
+}
