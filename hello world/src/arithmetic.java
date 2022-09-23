fe432271d11465866968b3302022e3e80947b4b1
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-23 08:55:41 -0300

    Create arithmetic.java

---
+++ hello world/src/arithmetic.java
@@ -0,0 +1,23 @@
+//  Faça um algoritmo que vai pedir dois números e depois apresentar os resultados para as operações de soma, subtração, multiplicação e divisão destes números.
+/**
+ * arithmetic
+ */
+import java.util.Scanner;
+public class arithmetic {
+    public static void main(String[] args) {
+        Scanner num1 = new Scanner(System.in);
+        Scanner num2 = new Scanner(System.in);
+        float x;
+        float y;
+        System.out.println("Digite um número: ");
+        x = Float.parseFloat(num1.nextLine());
+        System.out.println("Digite outro número: ");
+        y = Float.parseFloat(num2.nextLine());
+        System.out.println("A soma entre os números "+x+" e "+y+" é "+(x+y));
+        System.out.println("A soubtração entre os números "+x+" e "+y+" é "+(x-y));
+        System.out.println("A multiplicação entre os números "+x+" e "+y+" é "+(x*y));
+
+        num1.close();
+        num2.close();
+    }
+}
