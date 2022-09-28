21213414a597ad88a45bf71fda07bf46c7f5d6e1
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-28 10:58:25 -0300

    Calculadora

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         float x = 5f;
         float y = 3f;
-        System.out.println("Hello world! A soma é de : "+(x+y));
+        System.out.println("Hello world! A soma é de: "+(x+y));
     }
 }
