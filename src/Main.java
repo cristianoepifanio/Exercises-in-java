5d5c38e706814ca0a873f9771ee068cfa2d82435
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-28 11:09:45 -0300

    Calculadora

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         float x = 5f;
         float y = 3f;
         System.out.println("Hello world! A soma é de: "+(x+y));
+        System.out.println("A subtração é: "+(x-y));
     }
 }
