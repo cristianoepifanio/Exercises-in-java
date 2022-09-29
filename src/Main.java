1fdd9d1c29e2d51ee23ca1318165734d3eedb392
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2022-09-29 08:39:28 -0300

    Calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        float x = 5f;
-        float y = 3f;
-        System.out.println("Hello world! A soma é de: "+(x+y));
-        System.out.println("A subtração é: "+(x-y));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite x: ");
+        float x = scanner.nextFloat();
+        System.out.println("Digite y: ");
+        float y = scanner.nextFloat();
+        System.out.println("A soma entre "+x+" e "+y+" é de: "+(x+y));
+        System.out.println("A subtração entre "+x+" e "+y+" é de: "+(x-y));
+        System.out.println("A multiplicação entre "+x+" e "+y+" é de: "+(x*y));
     }
 }
