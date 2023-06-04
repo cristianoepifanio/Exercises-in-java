addab48c759c8bef13be2d974493c9c2771935b6
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2023-06-04 01:00:57 -0300

    created the option to choose the quetions.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,97 @@
 import java.util.Scanner;
 
 public class Main{
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        
-        // Resuloção da 1ª questão da 1ª lista do 1º módulo de lógica de programação
-                
-        algoritmos Mod1List1quest1 = new algoritmos();
-        Mod1List1quest1.calculadora();
-        
-        // Resuloção da 2ª questão da 1ª lista do 1º módulo de lógica de programação
-        
-        algoritmos Mod1List1quest2 = new algoritmos();
-        Mod1List1quest2.ICM();
 
-        try (Scanner sc = new Scanner(System.in)) {}
-    }
-   
+        // OBJECTS
+        Scanner scanner = new Scanner(System.in);
+        algoritmos answer = new algoritmos();
+        // VARIABLES
+        boolean QuetionsAgain = true; //repeat any of all the question
+        boolean QuetionAgain = true;  //repeat the running quetion
+        String repetir = "";
+        String repetirQuestao = "";
+
+        
+        
+        
+        while (QuetionsAgain == true){
+            QuetionAgain = true;
+            QuetionsAgain = true;
+            System.out.print("Digite o módulo: ");
+            int modulo = scanner.nextInt();
+            System.out.print("Qual Lista? ");
+            int lista = scanner.nextInt();
+            System.out.print("Qual questão? ");
+            int questao = scanner.nextInt();
+
+            if (modulo==1 && lista==1 && questao==1){
+                // Resuloção da 1ª questão da 1ª lista do 1º módulo de lógica de programação
+                while(QuetionAgain==true){ 
+                    answer.calculadora();
+                    // answer.repetirQuestao();
+
+                    // // To repeat the running quetion
+                    System.out.print("Deseja repetir a questão? ");
+                        repetirQuestao = scanner.next();
+                        
+                        
+
+                    switch(repetirQuestao){
+                        case "s":
+                        QuetionAgain = true;
+                        break;
+                        case "n":
+                        QuetionAgain = false;
+                        break;
+                        default:
+                        System.out.println("Por favor, digitar apenas 's' ou 'n'.");
+                    }
+                    
+                }        
+            }
+            if (modulo==1 && lista==1 && questao==2){
+                // Resuloção da 2ª questão da 1ª lista do 1º módulo de lógica de programação
+                while(QuetionAgain==true){ 
+                    answer.ICM();
+                    // answer.repetirQuestao();
+                    // // To repeat the running quetion
+                    System.out.print("Deseja repetir a questão? ");
+                    repetirQuestao = scanner.next();
+
+                    switch(repetirQuestao){
+                        case "s":
+                            QuetionAgain = true;
+                            break;
+                        case "n":
+                            QuetionAgain = false;
+                            break;
+                        default:
+                            while(repetirQuestao!="s"||repetirQuestao!="n" ){
+                                System.out.println("Por favor, digitar apenas 's' ou 'n'.");
+                            }
+                    }
+                    
+                }        
+            }
+
+            // to asking to repeat any other question
+            System.out.print("Deseja testar outra questão? (s/n)");
+            repetir = scanner.next();
+            switch(repetir){
+                case "s":
+                QuetionsAgain = true;
+                break;
+                case "n":
+                QuetionsAgain = false;
+                break;
+                default:
+                System.out.println("Por favor, digitar apenas 's' ou 'n'.");
+                
+            }
+        }
+   }
 }
--- src/algoritmos.java
+++ src/algoritmos.java
@@ -2,8 +2,27 @@ import java.util.Scanner;
 
 public class algoritmos {
 
+    Scanner sc = new Scanner(System.in);
+
+    // public void repetirQuestao(){
+    //     String repetirQuestao = "";
+    //     boolean QuetionAgain = true;
+    //     System.out.print("Deseja repetir a questão? ");
+    //                 repetirQuestao = sc.next();
+
+    //                 switch(repetirQuestao){
+    //                     case "s":
+    //                         QuetionAgain = true;
+    //                     break;
+    //                     case "n":
+    //                         QuetionAgain = false;
+    //                     break;
+    //                     default:
+    //                     System.out.println("Por favor, digitar apenas 's' ou 'n'.");
+    //                 }
+    // }
+    
     public void calculadora() {
-        Scanner sc = new Scanner(System.in);
             System.out.println("Digite num1: ");
             float x = sc.nextFloat();
             System.out.println("Digite num2: ");
@@ -11,27 +30,40 @@ public class algoritmos {
             float soma = x+y;
             float sub = x-y;
             float mult = x*y;
+            float div = x/y;
+
     
             System.out.println("A soma de "+x+" + "+y+" = "+soma);
-            System.out.println("A subtração de "+x+" + "+y+" = "+sub);
-            System.out.println("A multiplicação de "+x+" + "+y+" = "+mult);
+            System.out.println("A subtração de "+x+" - "+y+" = "+sub);
+            System.out.println("A multiplicação de "+x+" x "+y+" = "+mult);
+            if(y==0){
+                System.out.println("OPERAÇÃO INVÁLIDA: 'Não é definido divisão por 0.'");
+            }else{
+                System.out.println("A divisão de "+x+" / "+y+" = "+div);                
+            }
         
 
         
     }
     public void ICM(){
-        Scanner sc2 = new Scanner(System.in);
+        
             System.out.println("Digite seu peso: ");
-            float peso = sc2.nextFloat();
+            float peso = sc.nextFloat();
             System.out.println("Digite sua altura: ");
-            float altura = sc2.nextFloat();
+            float altura = sc.nextFloat();
             float icm = peso/(altura*altura);
             if(icm<18.5){
                 System.out.println("ABAIXO DO PESO.");
             }else if (icm<25){
                 System.out.println("PESO NORMAL.");
             }else if(icm<30){
-                System.out.println("sOBRE PESO.");
+                System.out.println("SOBRE PESO.");
+            }else if(icm<35){
+                System.out.println("OBESO LEVE.");
+            }else if(icm<=40){
+                System.out.println("OBESO MODERADO.");
+            }else if(icm>40){
+                System.out.println("OBESO MORBIDO.");
             }
 
     }
